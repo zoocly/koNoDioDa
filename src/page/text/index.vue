417f70af8bf310c8cac94473f59462dc78cb3b6b
clymene <326795631@qq.com>
2020-11-08 16:26:09 +0800

    1

--- src/page/text/index.vue
+++ src/page/text/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    我来看看情况
+    我来看看情况{{name}}
+    <pre>
+      function(){
+        return a = 123 但是pre 在react里面就回有问题
+      }
+    </pre>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      name: 'aaaaa'
+    }
+  },
+  mounted() {
+    let that=this;
+    setTimeout(function (){
+      that.name = 'bbbbb';
+    },1000)
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
