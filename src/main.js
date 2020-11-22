7f68a4544044bab91cab7ea384dcdaf8826dbb1c
clymene <326795631@qq.com>
2020-11-22 22:40:47 +0800

    增加axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from '../src/store'
+import request from "./untils/request";
 
 Vue.config.productionTip = false
 
@@ -15,6 +16,27 @@ new Vue({
   render:h=>h(App)
 })
 
+// 全局的设置 不好
+// axios({
+//   url:'http://123.207.32.32:8000/home/multidata',
+//   method:'get'
+// }).then(res=>{
+//   console.log(res);
+// })
 
-// runtime-compiler
+// 建立axios的实例
+// const instance = axios.create({
+//   baseURL:'http://123.207.32.32:8000',
+//   timeout:5000,
+// });
+// instance({
+//   url:'/home/multidata',
+//   method:'get'
+// })
 
+request({
+  url:'/home/multidata',
+  method:'get'
+}).then(res=>{
+  console.log(res,'res');
+}).catch()
---
+++ src/page/note/axios.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <pre>
+    jsonp 跨域
+
+    网络模块的封装
+
+    1 传统Ajax：基于 XMLHttpRequest
+      很少自己来封装，一般都是使用JQuery-Ajax
+    2 jquery-Ajax
+      在进行前端框架开发的时候，一般不使用jQuery来开发了
+    3 axios的优点
+      在浏览器中发送XMLHttpRequest请求
+      在node.js中发送http请求
+      支持promiseAPI
+      连接请求和响应
+      转换请求和相应数据
+
+    疑问1 XMLHttpRequest请求 和 http请求
+  </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "axios"
+}
+</script>
+
+<style scoped>
+
+</style>
